02d96d4d01e4fd51eaf4c87b8cc66d037e83f2da
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-04-02 20:39:40 -0400

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,58 @@
+class Board
+
+    attr_accessor :cells
+
+    def reset!
+       @cells =  Array.new(9, " ")
+    end
+
+    def initialize
+        @cells =  Array.new(9, " ")
+    end
+
+    def display
+        print " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+        print " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+        print " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+        print "-----------"
+    end
+
+    def position(position)
+        @cells[position.to_i - 1]
+    end
+
+    def full?
+        @cells.each do |cell|
+            if cell == " "
+                return false
+            end
+        end
+        true
+    end
+
+    def turn_count
+        turns = 0
+        @cells.each do |p|
+           if p == "X" || p == "O"
+            turns += 1
+           end
+        end
+        turns
+    end
+
+    def taken?(index) 
+        position(index) == " " ? false : true
+    end
+
+    def valid_move?(index)   
+        !(taken?(index)) && (0..9).include?(index.to_i - 1)
+    end
+
+    def update(position, player_token)
+
+        @cells[position.to_i - 1] = player_token.token
+    
+    end
+
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,19 @@
+module Players
+
+    class Human < Player
+
+
+        def move(board)
+            # board.display
+            puts "Please choose a number 1-9"
+            input = gets.strip
+            input
+        end
+
+
+    end
+
+
+
+end
+
